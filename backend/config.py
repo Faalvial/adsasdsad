91163f3573ddaffecc70cf7ae1b126ce9721bc64
Faalvial <farid23012006@gmail.com>
2026-07-04 11:07:21 -0500

    Agrega archivos de configuracion de Docker

--- backend/config.py
+++ backend/config.py
@@ -7,9 +7,11 @@ CAMERA_INDEX = "rtsp://admin:Hik12345@172.16.9.114:554/Streaming/Channels/101"
 SKIP_FRAMES = 5
 COOLDOWN = 15
 
+import os
+
 # PostgreSQL
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_NAME = "control_asistencia"
-DB_USER = "asistencia_user"
-DB_PASSWORD = "admin123"
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_PORT = int(os.environ.get("DB_PORT", 5432))
+DB_NAME = os.environ.get("DB_NAME", "control_asistencia")
+DB_USER = os.environ.get("DB_USER", "asistencia_user")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "admin123")
